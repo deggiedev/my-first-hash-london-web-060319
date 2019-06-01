a51e7c89ca0fb7d95eaa8d9652bcba57997a472c
Jack de Grave <jack.degrave@outlook.com>
2019-06-01 10:23:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,10 @@
 def my_hash
   chelsea_titles = {"Premiere League" => "2016/17", "premiere league" => "2015/15"}
-
-  
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
