525dfc16f7a41d7395fd95454fa04d23bb856adf
Jack de Grave <jack.degrave@outlook.com>
2019-06-01 10:20:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  chelsea_titles = {"Premiere League" => "2016/17", premi
 
   
 end
